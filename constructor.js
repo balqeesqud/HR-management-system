44dbc6b2cabbcd5500e476e6f5d98cc7c7b91849
balqees <balqeesalqudah97@gmail.com>
2023-06-17 10:40:55 +0300

    adding tofixed

--- constructor.js
+++ constructor.js
@@ -1,12 +1,9 @@
-
-// First Step: Create Constructor Function 
-
+// First Step: Create Constructor Function
 
 const Employees = [];
-let secEle= document.getElementById("secTag");  
+let secEle = document.getElementById('secTag');
 
-
-let formEle= document.getElementById("formEle");
+let formEle = document.getElementById('formEle');
 // let tableEle=document.getElementById("table");
 
 function Employee(employeeID, fullName, department, level, imageURL) {
@@ -19,38 +16,32 @@ function Employee(employeeID, fullName, department, level, imageURL) {
   Employees.push(this);
 }
 
+// Fourth Step: Create a Render Prototype Function To Render Each Employee Name With Their Salary
 
-// Fourth Step: Create a Render Prototype Function To Render Each Employee Name With Their Salary 
-
-// Render Method to render the data in the browser 
+// Render Method to render the data in the browser
 Employee.prototype.employeeRender = function () {
-  
-   // Update the employeeRender function by Dom Manipulation 
+  // Update the employeeRender function by Dom Manipulation
 
-   let divEle=document.createElement("div");
-   divEle.id="divElement";
-   secEle.appendChild(divEle);
+  let divEle = document.createElement('div');
+  divEle.id = 'divElement';
+  secEle.appendChild(divEle);
 
+  let imgEle = document.createElement('img');
+  imgEle.src = this.imageURL;
+  divEle.appendChild(imgEle);
 
-   let imgEle = document.createElement("img");
-   imgEle.src = this.imageURL;
-   divEle.appendChild(imgEle);
+  let h3Ele = document.createElement('h3');
+  h3Ele.textContent = `Employee Name: ${this.fullName} - ID: ${this.employeeID}
+    Department:  ${this.department} -  Level:  ${this.level}
+    ${this.salary}`;
 
-   let h3Ele = document.createElement("h3");
-   h3Ele.textContent = `Employee Name: ${this.fullName} - ID: ${this.employeeID}
-    Department:  ${this.department} -  Level:  ${this.level }
-    ${ this.salary}`
+  divEle.appendChild(h3Ele);
+};
 
-   divEle.appendChild(h3Ele);  
-
-  }
-
-function generateIDNumber(min,max){
-  let newId= Math.floor(Math.random() * (max - min + 1) + min)
+function generateIDNumber(min, max) {
+  let newId = Math.floor(Math.random() * (max - min + 1) + min);
   return newId;
- 
 }
-
 
 // Third Step: Create a Prototype Function For Calculating The Salary Based On Employee Level
 
@@ -58,17 +49,17 @@ Employee.prototype.generateRandomSalary = function () {
   let min, max;
 
   switch (this.level) {
-    case "Junior":
+    case 'Junior':
       min = 500;
       max = 1000;
       break;
 
-    case "Mid-Senior":
+    case 'Mid-Senior':
       min = 1000;
       max = 1500;
       break;
 
-    case "Senior":
+    case 'Senior':
       min = 1500;
       max = 2000;
       break;
@@ -77,91 +68,126 @@ Employee.prototype.generateRandomSalary = function () {
   var randomSalary = Math.floor(Math.random() * (max - min)) + min;
   var taxPercent = 7.5;
   var taxAmount = (randomSalary * taxPercent) / 100;
-  this.salary = (randomSalary - taxAmount).toFixed(2);
- // Math.floor removes fractions and round the number to the lowest value. 
+  this.salary = randomSalary - taxAmount;
+  // Math.floor removes fractions and round the number to the lowest value.
 };
-
 
 // Second Step: Create New Objects (creating instances of the Employee constructor) // theses called hard copy data
 
-let employeeOne = new Employee(1000, "Ghazi Samer", "Administration", "Senior", "./assests/Ghazi.jpg");
-let employeeTwo = new Employee(1001, "Lana Ali", "Finance", "Senior", "assests/Lana.jpg");
-let employeeThree = new Employee(1002, "Tamara Ayoub", "Marketing", "Senior", "assests/Tamara.jpg" );
-let employeeFour = new Employee(1003, "Safi Walid", "Administration", "Mid-Senior", "assests/Safi.jpg" );
-let employeeFive = new Employee(1004, "Omar Zaid", "Development", "Senior", "assests/Omar.jpg");
-let employeeSix = new Employee(1005, "Rana Saleh", "Development", "Junior", "assests/Rana.jpg");
-let employeeSeven = new Employee(1006, "Hadi Ahmad", "Finance", "Mid-Senior","assests/Hadi.jpg" );
+let employeeOne = new Employee(
+  1000,
+  'Ghazi Samer',
+  'Administration',
+  'Senior',
+  './assests/Ghazi.jpg'
+);
+let employeeTwo = new Employee(
+  1001,
+  'Lana Ali',
+  'Finance',
+  'Senior',
+  'assests/Lana.jpg'
+);
+let employeeThree = new Employee(
+  1002,
+  'Tamara Ayoub',
+  'Marketing',
+  'Senior',
+  'assests/Tamara.jpg'
+);
+let employeeFour = new Employee(
+  1003,
+  'Safi Walid',
+  'Administration',
+  'Mid-Senior',
+  'assests/Safi.jpg'
+);
+let employeeFive = new Employee(
+  1004,
+  'Omar Zaid',
+  'Development',
+  'Senior',
+  'assests/Omar.jpg'
+);
+let employeeSix = new Employee(
+  1005,
+  'Rana Saleh',
+  'Development',
+  'Junior',
+  'assests/Rana.jpg'
+);
+let employeeSeven = new Employee(
+  1006,
+  'Hadi Ahmad',
+  'Finance',
+  'Mid-Senior',
+  'assests/Hadi.jpg'
+);
 
-
-formEle.addEventListener("submit", submitHandler);
+formEle.addEventListener('submit', submitHandler);
 
 function submitHandler(event) {
-  event.preventDefault(); 
+  event.preventDefault();
 
-  let department=event.target.department.value;
-  let level=event.target.level.value;
-  let fullName=event.target.fullName.value;
+  let department = event.target.department.value;
+  let level = event.target.level.value;
+  let fullName = event.target.fullName.value;
   let img = event.target.img.value;
-  let id= generateIDNumber(1000,9000); 
- 
-
+  let id = generateIDNumber(1000, 9000);
 
   console.log(fullName);
-  console.log(department); 
+  console.log(department);
   console.log(level);
   console.log(img);
-  console.log(id); 
-  
- // Employee(employeeID, fullName, department, level, imageURL)
-  let newEmployee= new Employee(id, fullName, department, level, img);
-  newEmployee.employeeRender(); 
-  console.log("array after submit event", Employees );
+  console.log(id);
+
+  // Employee(employeeID, fullName, department, level, imageURL)
+  let newEmployee = new Employee(id, fullName, department, level, img);
+  newEmployee.employeeRender();
+  console.log('array after submit event', Employees);
   saveData(Employees);
 }
 
-console.log("before saving in the LS",Employees);
+console.log('before saving in the LS', Employees);
 
 // console.log(employeeOne);
 // console.log(employeeTwo);
 // console.log(Employees);
 
-
 function saveData(data) {
-  let stringArr = JSON.stringify(data); // array of stings 
+  let stringArr = JSON.stringify(data); // array of stings
   localStorage.setItem('employees', stringArr);
-
 }
 
 function getData() {
-let retrievedArr= localStorage.getItem('employees'); // array of strings 
-console.log("data is coming from LS as strings", retrievedArr);
+  let retrievedArr = localStorage.getItem('employees'); // array of strings
+  console.log('data is coming from LS as strings', retrievedArr);
 
-let objArr=JSON.parse(retrievedArr); // array of objects
-console.log("data is coming from LS as objects",objArr);
+  let objArr = JSON.parse(retrievedArr); // array of objects
+  console.log('data is coming from LS as objects', objArr);
 
-//(employeeID, fullName, department, level, imageURL) should be arranged as the constructor 
+  //(employeeID, fullName, department, level, imageURL) should be arranged as the constructor
 
-if (objArr !=null){
-  for (let i=7; i<objArr.length; i++){ //iterating through objArr array that is coming from LS and starting from the new object index ; create new instance
-    new Employee(objArr[i].employeeID, objArr[i].fullName, objArr[i].department, objArr[i].level, objArr[i].imageURL);
-  } 
-}
+  if (objArr != null) {
+    for (let i = 7; i < objArr.length; i++) {
+      //iterating through objArr array that is coming from LS and starting from the new object index ; create new instance
+      new Employee(
+        objArr[i].employeeID,
+        objArr[i].fullName,
+        objArr[i].department,
+        objArr[i].level,
+        objArr[i].imageURL
+      );
+    }
+  }
 
   renderAll();
-
 }
 getData();
 
-
-
-function renderAll(){
-
-for (let i = 0; i < Employees.length; i++) {
-  Employees[i].generateRandomSalary();
-  Employees[i].employeeRender();
-  
+function renderAll() {
+  for (let i = 0; i < Employees.length; i++) {
+    Employees[i].generateRandomSalary();
+    Employees[i].employeeRender();
+  }
 }
-
-}
-
-
